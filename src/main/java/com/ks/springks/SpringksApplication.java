257894bf68770009e566ba960d4dc1f5d64beeeb
animireddy-san <animireddysantosh@gmail.com>
2023-06-19 09:10:35 +0530

    first commit

---
+++ src/main/java/com/ks/springks/SpringksApplication.java
@@ -0,0 +1,13 @@
+package com.ks.springks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ks/springks/SpringksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ks.springks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
